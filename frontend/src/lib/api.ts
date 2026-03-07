8f9d9d1e132572bcad2910d59ecebbb23237d467
Rvk <borahjyotirmoy030@gmail.com>
2026-03-07 21:38:31 +0530

    fix api url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/api` : 'http://localhost:5000/api';
-
+const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:5000/api';
 export const api = axios.create({
     baseURL: API_URL,
     headers: { 'Content-Type': 'application/json' },
